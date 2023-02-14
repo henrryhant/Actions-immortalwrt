6b9a8e98c10945ec07748f4b346442cb0c72dbe5
henrryhant <60613880+henrryhant@users.noreply.github.com>
2023-02-15 07:14:20 +0800

    Update diy-immortalwrt-part2.sh
    2023021501

--- diy-immortalwrt-part2.sh
+++ diy-immortalwrt-part2.sh
@@ -18,8 +18,9 @@ sed -i 's/192.168.1.1/192.168.9.1/g' package/base-files/files/bin/config_generat
 
 ########### 更新smartdns版本 ###########
 sed -i "/^PKG_VERSION:=/cPKG_VERSION:=1.2022.40" feeds/packages/net/smartdns/Makefile
+sed -i "/^PKG_RELEASE/cPKG_RELEASE:=1" feeds/packages/net/smartdns/Makefile
 sed -i "/^PKG_SOURCE:=/cPKG_SOURCE_PROTO:=git" feeds/packages/net/smartdns/Makefile
 sed -i "/^PKG_SOURCE_URL:=/cPKG_SOURCE_URL:=https://www.github.com/pymumu/smartdns.git" feeds/packages/net/smartdns/Makefile
 sed -i "/^PKG_HASH:=/cPKG_SOURCE_VERSION:=db56472b845b12d47f59e63aa8411bdca3a6f004" feeds/packages/net/smartdns/Makefile
 sed -i "s/PKG_BUILD_DIR/#PKG_BUILD_DIR/" feeds/packages/net/smartdns/Makefile
-sed -i "50i     \$(INSTALL_DIR) \$(1)/etc/smartdns \$(1)/etc/smartdns/domain-set \$(1)/etc/smartdns/conf.d/" feeds/packages/net/smartdns/Makefile
+sed -i "50i        \$(INSTALL_DIR) \$(1)/etc/smartdns \$(1)/etc/smartdns/domain-set \$(1)/etc/smartdns/conf.d/" feeds/packages/net/smartdns/Makefile
