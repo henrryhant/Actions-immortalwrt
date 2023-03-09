6e1cbca326f2296355317118f443b5ad21c6e091
henrryhant <60613880+henrryhant@users.noreply.github.com>
2023-03-09 13:33:35 +0800

    Update diy-immortalwrt-part2.sh
    202303091333

--- diy-immortalwrt-part2.sh
+++ diy-immortalwrt-part2.sh
@@ -17,10 +17,14 @@ sed -i 's/192.168.1.1/192.168.9.1/g' package/base-files/files/bin/config_generat
 #sed -i 's@.*CYXluq4wUazHjmCDBCqXF*@#&@g' package/lean/default-settings/files/zzz-default-settings
 
 ########### 更新smartdns版本 ###########
-#sed -i "/^PKG_VERSION:=/cPKG_VERSION:=1.2023.40" feeds/packages/net/smartdns/Makefile
-#sed -i "/^PKG_RELEASE/cPKG_RELEASE:=1" feeds/packages/net/smartdns/Makefile
-#sed -i "/^PKG_SOURCE:=/cPKG_SOURCE_PROTO:=git" feeds/packages/net/smartdns/Makefile
-#sed -i "/^PKG_SOURCE_URL:=/cPKG_SOURCE_URL:=https://www.github.com/pymumu/smartdns.git" feeds/packages/net/smartdns/Makefile
-#sed -i "/^PKG_HASH:=/cPKG_SOURCE_VERSION:=499ab1b64f2de4985eb4283d992dee01fa2ca05c" feeds/packages/net/smartdns/Makefile
-#sed -i "s/PKG_BUILD_DIR/#PKG_BUILD_DIR/" feeds/packages/net/smartdns/Makefile
-#sed -i "50i        \$(INSTALL_DIR) \$(1)/etc/smartdns \$(1)/etc/smartdns/domain-set \$(1)/etc/smartdns/conf.d/" feeds/packages/net/smartdns/Makefile
+rm -rf feeds/packages/net/smartdns
+git clone https://github.com/pymumu/openwrt-smartdns.git feeds/packages/net/smartdns
+sed -i "/^PKG_SOURCE_VERSION:=/cPKG_SOURCE_VERSION:=9a43f0859d6f75bad979f116b48723a50453ad09" feeds/packages/net/smartdns/Makefile
+rm -rf feeds/luci/applications/luci-app-smartdns
+git clone https://github.com/pymumu/luci-app-smartdns.git feeds/luci/applications/luci-app-smartdns
+
+rm -rf feeds/luci/applications/luci-app-passwall
+cd feeds/luci/applications
+git clone -b luci-smartdns-new-version https://github.com/xiaorouji/openwrt-passwall.git
+cp -r openwrt-passwall/luci-app-passwall ./
+rm -rf openwrt-passwall
