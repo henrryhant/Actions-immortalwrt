a854a338229ac66626e4d1180c119364a0dd73cc
henrryhant <60613880+henrryhant@users.noreply.github.com>
2023-03-11 19:16:47 +0800

    Update diy-immortalwrt-part2.sh
    202303111916

--- diy-immortalwrt-part2.sh
+++ diy-immortalwrt-part2.sh
@@ -19,12 +19,9 @@ sed -i 's/192.168.1.1/192.168.9.1/g' package/base-files/files/bin/config_generat
 ########### 更新smartdns版本 ###########
 rm -rf feeds/packages/net/smartdns
 git clone https://github.com/pymumu/openwrt-smartdns.git feeds/packages/net/smartdns
-sed -i "/^PKG_SOURCE_VERSION:=/cPKG_SOURCE_VERSION:=1ac2b2ad98bef602bc7f4631ae3f84cb448fa4ee" feeds/packages/net/smartdns/Makefile
+sed -i "/^PKG_SOURCE_VERSION:=/cPKG_SOURCE_VERSION:=3c7b86eea755cf726a49d452a7b43ceeb01f2a06" feeds/packages/net/smartdns/Makefile
 rm -rf feeds/luci/applications/luci-app-smartdns
 git clone https://github.com/pymumu/luci-app-smartdns.git feeds/luci/applications/luci-app-smartdns
 
 rm -rf feeds/luci/applications/luci-app-passwall
-cd feeds/luci/applications
-git clone -b luci-smartdns-new-version https://github.com/xiaorouji/openwrt-passwall.git
-cp -r openwrt-passwall/luci-app-passwall ./
-rm -rf openwrt-passwall
+svn export https://github.com/xiaorouji/openwrt-passwall/branches/luci-smartdns-new-version/luci-app-passwall feeds/luci/applications/luci-app-passwall
